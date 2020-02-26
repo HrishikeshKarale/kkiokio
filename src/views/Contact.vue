d4a962a7c2761a8245218488546f11ef51311a05
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2020-02-26 06:45:08 -0500

    contact and about me page created (template)

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,107 @@
 <template>
   <div class="contact">
-    <h1>This is an Contact page</h1>
+    <div class="title">
+      <h1>Contact Me</h1>
+      <p>
+        Hello, I am located in Upstate New York area.
+        <br />
+        Feel free to use the contact form below and reach out to me for any
+        questions yo might have.
+        <br />
+        You can also write to me the old fashion way.
+      </p>
+    </div>
+    <div class="body">
+      <div class="contactDetails">
+        <div class="address">
+          <h3>Postal Address</h3>
+          <p>
+            44 Crittenden Way,
+            <br />
+            Apartment 2,
+            <br />
+            Rochester, NY 14623
+          </p>
+        </div>
+        <div class="email">
+          <h3>Email</h3>
+          hrishikesh.karale@gmai.com
+        </div>
+        <div class="phone">
+          <h3>Phone Number</h3>
+          <p>
+            Hours: 8am - 4pm (EST), everyday
+            <br />
+            Text messages prefered.
+          </p>
+          <h4>(585) 520-7382</h4>
+          <i>
+            *First time caller(s) are screened using virtual assistan<br />to
+            weed out spam calls</i
+          >
+        </div>
+      </div>
+      <div class="contactForm">
+        <form
+          action="mailto:hrishikesh.karale@gmail.com"
+          method="post"
+          enctype="text/plain"
+        >
+          Name:<br />
+          <input type="text" name="name" /><br />
+          E-mail:<br />
+          <input type="text" name="mail" /><br />
+          Phone number:<br />
+          <input type="text" name="mail" /><br />
+          Message:<br />
+          <input type="text" name="comment" size="50" /><br /><br />
+          <input type="submit" value="Send" />
+          <input type="reset" value="Reset" />
+        </form>
+      </div>
+    </div>
   </div>
 </template>
+<style lang="less" scoped>
+@import (reference) "./../Less/customMixins.less";
+@import (reference) "./../Less/customVariables.less";
+
+.contact {
+  display: flex;
+  flex-direction: column;
+
+  & > div {
+    &.title {
+      & > h1 {
+        text-align: center;
+        & + p {
+          font-size: 18px;
+          text-align: center;
+        }
+      }
+    }
+    &.body {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      & > div {
+        display: flex;
+        flex-direction: column;
+        max-width: 40vw;
+        min-width: 24opx;
+        &.contactDetails {
+          & > div {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+          }
+        }
+
+        &.contactForm {
+          display: flex;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -41,51 +41,6 @@
         </a>
       </div>
     </div>
-    <article id="aboutMe">
-      <h1>About Me</h1>
-      <div>
-        <h3>Hrishikesh Karale</h3>
-        <h3>UX Professional</h3>
-        <h4>Full Stack Designer</h4>
-        <h4>Born 14th Feb, 1990</h4>
-      </div>
-      <div class="bio">
-        <div>
-          <img alt="profile Picture" src="../../public/img/profilePic.jpg" />
-        </div>
-        <div>
-          <p>
-            I'm a UX Professional with 3+ years of experience working on several
-            major software projects in healthcare, eCommerce and enterprise
-            systems. My primary objectives are to design interfaces that can
-            solve problems and create experiences that initiate a sense of
-            delight and wonder for my users.
-          </p>
-          <p>
-            Having both a Design and Development background allows me to balance
-            both the needs of the end-user as well as the business goals of the
-            company. As a UX designer, I strive to use a constant end-user
-            feedback loop to define and redefine my project objectives. The
-            user's needs guide the strategic decisions I make during my UX
-            process. This non-linear approach is integral to ensuring that my
-            design decisions lead to a massive improvement in the key metrics
-            that are relevant to the user experience. I also strictly make sure
-            that my design decisions can be implemented in the final product
-            without compromising the project timeline. In short, I like to think
-            like an engineer, data-scientist and marketer in addition to being a
-            UX designer.
-          </p>
-          <p>
-            I’m also a novice visual designer. I’ve designed over 30 working
-            prototypes in online-shopping, warehouse-management,
-            inventory-management, fulfillment and healthcare using a multitude
-            of prototyping tools including products from Adobe Creative Cloud,
-            Sketch and Figma. My prototyping strategy primarily focuses on
-            functionality and usability.
-          </p>
-        </div>
-      </div>
-    </article>
   </div>
 </template>
 
@@ -151,40 +106,6 @@ div {
             & > abbr {
               color: #fbce51;
               text-decoration: none;
-            }
-          }
-        }
-      }
-    }
-    & > article {
-      &#aboutMe {
-        display: flex;
-        flex-direction: column;
-        margin-top: 8vh;
-        & > div {
-          display: flex;
-          flex-direction: column;
-          flex-wrap: wrap;
-          &:not(.bio) > div {
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-          }
-          &.bio {
-            flex-direction: row;
-            flex-wrap: wrap;
-            justify-content: space-around;
-            & > div {
-              margin: 8vh 0 8vh 0;
-              & > img {
-                max-height: 60vh;
-                min-height: 320px;
-              }
-              & > p {
-                max-width: 40vw;
-                min-width: 480px;
-                font-size: 18px;
-              }
             }
           }
         }
