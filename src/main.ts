3fc9968a701cddf9dafd6388f4baa1811c43b2f2
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2020-10-07 19:57:53 -0400

    global and component event lister instructions added to main .ts file

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,23 @@ import App from "@/App.vue";
 Vue.config.productionTip = false;
 
 /*
+global eventbus
 link: https://medium.com/vuejobs/create-a-global-event-bus-in-vue-js-838a5d9ab03a
+emitting an event: this.$globalEventBus.$emit('change-name', this.newName);
+catching an event: this.$globalEventBus.$on('change-name', this.changeName)
+destroy event: this.$globalEventBus.$off('change-name');
 
-emitting an event: this.$eventHub.$emit('change-name', this.newName);
-
-catching an event: this.$eventHub.$on('change-name', this.changeName)
-
-destroy event: this.$eventHub.$off('change-name');
+component eventbus:
+link: https://blog.logrocket.com/using-event-bus-in-vue-js-to-pass-data-between-components/
+initialization; import eventbus from "@/store/eventBus"
+emmit an event: eventbus.$emit('changeIt', 'changed header');
+catching an event: eventbus.$on('changeIt', (data) => {
+                    this.header = data;
+                  })
+destroy event: eventbus.$off();
+link the catch event to lifecycle hook to catch the event better
 */
-Vue.prototype.$eventHub = new Vue(); // Global event bus
+Vue.prototype.$globalEventBus = new Vue(); // Global event bus
 
 new Vue({
   router,
