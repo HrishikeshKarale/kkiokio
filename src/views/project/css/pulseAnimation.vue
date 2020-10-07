9898a33453d1559fa441787dcd510ae879c3c370
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2020-10-07 18:11:52 -0400

    pulse animation added to circle

--- src/views/project/css/pulseAnimation.vue
+++ src/views/project/css/pulseAnimation.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="pulseAnimation">
     pulseAnimation
+    <div class="pulse" />
   </div>
 </template>
 <script>
@@ -19,5 +20,31 @@ export default {
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-around;
+  & > .pulse {
+    border-radius: 50%;
+    width: @fontSize;
+    height: @fontSize;
+    background: blue;
+    &::after {
+      content: "";
+      position: absolute;
+      width: @fontSize;
+      height: @fontSize;
+      z-index: 1;
+      border-radius: 50%;
+      background: blue;
+      animation: pulse 1s ease infinite;
+    }
+  }
+  @keyframes pulse {
+    from {
+      transform: scale(1);
+      opacity: 1;
+    }
+    to {
+      transform: scale(2);
+      opacity: 0;
+    }
+  }
 }
 </style>
