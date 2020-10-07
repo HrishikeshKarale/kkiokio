d58abf8a76a6a9882618a0aff2135a39bb73270e
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2020-10-07 19:48:55 -0400

    creating a global  eventbus

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,22 @@ import store from "@/store";
 import "@/Less/fontAwesome/fontawesome.less";
 import "@/Less/bootstrap.less";
 import App from "@/App.vue";
-// import EventBus from "./store/eventBus";
 
 Vue.config.productionTip = false;
+
+/*
+link: https://medium.com/vuejobs/create-a-global-event-bus-in-vue-js-838a5d9ab03a
+
+emitting an event: this.$eventHub.$emit('change-name', this.newName);
+
+catching an event: this.$eventHub.$on('change-name', this.changeName)
+
+destroy event: this.$eventHub.$off('change-name');
+*/
+Vue.prototype.$eventHub = new Vue(); // Global event bus
 
 new Vue({
   router,
   store,
-  // EventBus,
   render: h => h(App)
 }).$mount("#app");
