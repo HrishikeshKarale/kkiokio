d1a00a894718b495d958504387573c7efe9ffb7a
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2020-10-08 13:48:44 -0400

    start stop audio video needs work

--- src/views/project/js/userMedia.vue
+++ src/views/project/js/userMedia.vue
@@ -7,6 +7,15 @@
     <!-- <input type="file" accept="image/*;capture=camera" />
     <input type="file" accept="video/*;capture=camcorder" />
     <input type="file" accept="audio/*;capture=microphone" /> -->
+    <div class="select">
+      <label for="audioSource">Audio source: </label>
+      <select ref="audioSource" id="audioSource"></select>
+    </div>
+
+    <div class="select">
+      <label for="videoSource">Video source: </label>
+      <select ref="videoSource" id="videoSource"></select>
+    </div>
     <video autoplay />
   </div>
 </template>
@@ -14,6 +23,8 @@
 export default {
   name: "userMedia",
   data() {
+    let audioSelect;
+    let videoSelect;
     const constraints = {
       audio: true,
       video: {
@@ -22,56 +33,119 @@ export default {
       }
     };
     return {
+      audioSelect,
+      videoSelect,
       constraints
     };
   },
   methods: {
+    // stop both mic and camera
+    stopBothVideoAndAudio: function(stream) {
+      stream.getTracks().forEach(function(track) {
+        if (track.readyState == "live") {
+          track.stop();
+        }
+      });
+    }, //stopBothVideoAndAudio
+
+    // stop only camera
+    stopVideoOnly: function(stream) {
+      stream.getTracks().forEach(function(track) {
+        if (track.readyState == "live" && track.kind === "video") {
+          track.stop();
+        }
+      });
+    }, //stopVideoOnly
+
+    // stop only mic
+    stopAudioOnly: function(stream) {
+      stream.getTracks().forEach(function(track) {
+        if (track.readyState == "live" && track.kind === "audio") {
+          track.stop();
+        }
+      });
+    }, //stopAudioOnly
+
     hasGetUserMedia: function() {
-      return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+      return navigator.mediaDevices && navigator.mediaDevices.getUserMedia;
     }, //hasGetUserMedia
 
+    sortMedia: function() {
+      navigator.mediaDevices
+        .enumerateDevices() //get list of devices
+        .then(deviceInfos => {
+          this.audioSelect = this.$refs.audioSource;
+          this.videoSelect = this.$refs.videoSource;
+          for (let i = 0; i !== deviceInfos.length; ++i) {
+            const deviceInfo = deviceInfos[i];
+            const option = document.createElement("option");
+            option.value = deviceInfo.deviceId;
+            //checks for audio source
+            if (deviceInfo.kind === "audioinput") {
+              option.text =
+                deviceInfo.label ||
+                "microphone " + (this.audioSelect.length + 1);
+              this.audioSelect.appendChild(option);
+            }
+            //checks for video source
+            else if (deviceInfo.kind === "videoinput") {
+              option.text =
+                deviceInfo.label || "camera " + (this.videoSelect.length + 1);
+              this.videoSelect.appendChild(option);
+            } else {
+              console.log(
+                deviceInfo.label
+                  ? "Found another device: " + deviceInfo.label
+                  : "Found another device of kind/type" +
+                      deviceInfo.kind +
+                      +" : " +
+                      deviceInfo
+              );
+            }
+          }
+          this.openStream();
+        })
+        .catch();
+    }, //sortMedia
+
+    //opens streaming for all devices and pauses it
+    openStream: function() {
+      navigator.mediaDevices.getUserMedia(this.constraints).then(stream => {
+        stream.getTracks().forEach(function(track) {
+          track.stop();
+        });
+      });
+      this.constraints = {
+        audio: {
+          deviceId: { exact: this.audioSelect.value }
+        },
+        video: {
+          deviceId: { exact: this.videoSelect.value }
+        }
+      };
+      // navigator.mediaDevices
+      //   .getUserMedia(constraints)
+      //   .then(gotStream)
+      //   .catch(handleError);
+    }, //openStream
+
     getMedia: function() {
-      if (this.hadGetUserMedia || true == true) {
+      const hasMedia = this.hasGetUserMedia();
+      if (hasMedia) {
+        this.sortMedia();
         const video = document.querySelector("video");
         navigator.mediaDevices.getUserMedia(this.constraints).then(stream => {
           video.srcObject = stream;
         });
       } else {
-        console.log("media access not found");
+        console.error("media access not found");
         //do something here in case of media not found
       }
     } //getMedia
-
-    // getMedia: function(
-    //   constraints = {
-    //     audio: true,
-    //     video: {
-    //       width: { min: 1280 },
-    //       height: { min: 720 }
-    //     }
-    //   }
-    // ) {
-    //   try {
-    //     navigator.mediaDevices
-    //       .getUserMedia(constraints)
-    //       .then(function(mediaStream) {
-    //         const video = document.querySelector("video");
-    //         video.srcObject = mediaStream;
-    //         video.onloadedmetadata = () => {
-    //           video.play();
-    //         };
-    //       })
-    //       .catch(function(err) {
-    //         console.log(err.name + ": " + err.message);
-    //       }); // always check for errors at the end.
-    //     /* use the stream */
-    //   } catch (err) {
-    //     //catch statement here
-    //   }
-    // }
   }, //methods
 
   mounted() {
+    // this.sortMedia();
     this.getMedia();
   }
 };
@@ -85,5 +159,8 @@ export default {
   // justify-content: space-around;
   width: 100%;
   height: 100%;
+  & > video {
+    height: 480px;
+  }
 }
 </style>
