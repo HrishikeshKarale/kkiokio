49cb8178c666481d7b4159091af43e0fa3293672
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2021-08-29 13:11:58 -0400

    responsive minimizing header

--- src/components/enterpriseAppLayout.vue
+++ src/components/enterpriseAppLayout.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="enterpriseAppLayout">
-    <template v-if="$slots['header']">
-      <slot name="header" />
-    </template>
-    <div class="body">
-      <template v-if="$slots['menu']">
-        <slot name="menu" />
-      </template>
-      <transition
-        :name="transitionName"
-        :mode="transitionMode"
-        :enter-active-class="transitionEnterActiveClass"
-        @beforeLeave="beforeLeave"
-        @enter="enter"
-        @afterEnter="afterEnter"
-      >
-        <div :key="$route.path" class="content">
-          <countdown-timer
-            class="alert"
-            start-time="January 24, 2021 23:59:99"
-            end-time="Aug 16, 2021 00:00:01"
-            trans='{
+	<div class="enterpriseAppLayout">
+		<template v-if="$slots['header']">
+			<slot name="header" />
+		</template>
+		<div class="body">
+			<template v-if="$slots['menu']">
+				<slot name="menu" />
+			</template>
+			<transition
+				:name="transitionName"
+				:mode="transitionMode"
+				:enter-active-class="transitionEnterActiveClass"
+				@beforeLeave="beforeLeave"
+				@enter="enter"
+				@afterEnter="afterEnter"
+			>
+				<div :key="$route.path" class="content">
+					<countdown-timer
+						class="alert"
+						start-time="January 24, 2021 23:59:99"
+						end-time="Aug 16, 2021 00:00:01"
+						trans='{
               "day":"Day",
               "hours":"Hours",
               "minutes":"Minuts",
@@ -34,270 +34,270 @@
                 "upcoming":"Future"
               }
             }'
-          />
-          <breadcrums />
-          <scroll-indicator>
-            <router-view :key="$route.path" />
-          </scroll-indicator>
-          <template v-if="$slots['moto']">
-            <slot name="moto" />
-          </template>
-        </div>
-      </transition>
-      <template v-if="$slots['footer']">
-        <slot name="footer" />
-      </template>
-    </div>
-  </div>
+					/>
+					<breadcrums />
+					<scroll-indicator>
+						<router-view :key="$route.path" />
+					</scroll-indicator>
+					<template v-if="$slots['moto']">
+						<slot name="moto" />
+					</template>
+				</div>
+			</transition>
+			<template v-if="$slots['footer']">
+				<slot name="footer" />
+			</template>
+		</div>
+	</div>
 </template>
 
 <script>
-import scrollIndicator from "@/views/project/js/scrollIndicator/scrollIndicator";
-import CountdownTimer from "@/components/countdownTimer.vue";
-import breadcrums from "@/components/breadcrums";
-import { authentication } from "@/typeScript/authentication";
-import { cookie } from "@/typeScript/cookie";
+	import scrollIndicator from "@/views/project/js/scrollIndicator/scrollIndicator";
+	import CountdownTimer from "@/components/countdownTimer.vue";
+	import breadcrums from "@/components/breadcrums";
+	import { authentication } from "@/typeScript/authentication";
+	import { cookie } from "@/typeScript/cookie";
 
-export default {
-  name: "EnterpriseAppLayout",
-  components: {
-    CountdownTimer,
-    scrollIndicator,
-    breadcrums
-  },
+	export default {
+		name: "EnterpriseAppLayout",
+		components: {
+			CountdownTimer,
+			scrollIndicator,
+			breadcrums,
+		},
 
-  mixins: [authentication, cookie],
-  data() {
-    const DEFAULT_TRANSITION = "fade";
-    const DEFAULT_TRANSITION_MODE = "out-in";
-    const transitionEnterActiveClass = "";
-    const prevHeight = 0;
-    return {
-      transitionName: DEFAULT_TRANSITION,
-      transitionMode: DEFAULT_TRANSITION_MODE,
-      transitionEnterActiveClass,
-      prevHeight
-    };
-  }, //mixins
+		mixins: [authentication, cookie],
+		data() {
+			const DEFAULT_TRANSITION = "fade";
+			const DEFAULT_TRANSITION_MODE = "out-in";
+			const transitionEnterActiveClass = "";
+			const prevHeight = 0;
+			return {
+				transitionName: DEFAULT_TRANSITION,
+				transitionMode: DEFAULT_TRANSITION_MODE,
+				transitionEnterActiveClass,
+				prevHeight,
+			};
+		}, //mixins
 
-  beforeMount() {
-    this.$router.beforeEach((to, from, next) => {
-      let transitionName =
-        to.meta.transitionName ||
-        from.meta.transitionName ||
-        this.transitionName;
-      if (transitionName === "slide") {
-        const toDepth = to.path.split("/").length;
-        const fromDepth = from.path.split("/").length;
-        transitionName = toDepth < fromDepth ? "slide-right" : "slide-left";
-      }
-      // this.transitionMode = DEFAULT_TRANSITION_MODE;
-      this.transitionEnterActiveClass = `${transitionName}-enter-active`;
+		beforeMount() {
+			this.$router.beforeEach((to, from, next) => {
+				let transitionName =
+					to.meta.transitionName ||
+					from.meta.transitionName ||
+					this.transitionName;
+				if (transitionName === "slide") {
+					const toDepth = to.path.split("/").length;
+					const fromDepth = from.path.split("/").length;
+					transitionName = toDepth < fromDepth ? "slide-right" : "slide-left";
+				}
+				// this.transitionMode = DEFAULT_TRANSITION_MODE;
+				this.transitionEnterActiveClass = `${transitionName}-enter-active`;
 
-      if (to.meta.transitionName === "zoom") {
-        this.transitionMode = "in-out";
-        this.transitionEnterActiveClass = "zoom-enter-active";
-        // Disable scrolling in the background.
-        document.body.style.overflow = "hidden";
-      }
+				if (to.meta.transitionName === "zoom") {
+					this.transitionMode = "in-out";
+					this.transitionEnterActiveClass = "zoom-enter-active";
+					// Disable scrolling in the background.
+					document.body.style.overflow = "hidden";
+				}
 
-      if (from.meta.transitionName === "zoom") {
-        this.transitionMode = null;
-        this.transitionEnterActiveClass = null;
-        // Enable scrolling again.
-        document.body.style.overflow = null;
-      }
+				if (from.meta.transitionName === "zoom") {
+					this.transitionMode = null;
+					this.transitionEnterActiveClass = null;
+					// Enable scrolling again.
+					document.body.style.overflow = null;
+				}
 
-      this.transitionName = transitionName;
+				this.transitionName = transitionName;
 
-      //check if matched route requires authentication
-      if (to.matched.some(record => record.meta.requiresAuth)) {
-        //if matched route requires authentication then check for absence of token
-        if (localStorage.getItem("jwt") == null && !this.checkCookie("token")) {
-          //when no token is found redirect to login page and set redirec
-          next({
-            name: "login",
-            query: { nextUrl: to.name }
-          });
-        }
-        //if matched route requires authentication and has token
-        else {
-          let user = "{}";
-          if (localStorage.getItem("user")) {
-            user = localStorage.getItem("user");
-          } else if (this.checkCookie("user")) {
-            //console.log(this.getCookie('user'));
-            user = this.getCookie("user");
-          }
-          // const user = JSON.parse(localStorage.getItem('user') || JSON.parse(this.getCookie('user')) ||{});
-          //when token is present check if user is an Admin
-          if (to.matched.some(record => record.meta.isAdmin)) {
-            //If user is an admin, proceed
-            if (user.isAdmin == 1) {
-              next();
-            }
-            //if user is not admin then redirect to  about page
-            else {
-              next({ path: "home" });
-            }
-          } else {
-            next();
-          }
-        }
-      }
-      //authentication was not required.
-      //check if guest access is required to matched route
-      else if (to.matched.some(record => record.meta.guest)) {
-        if (localStorage.getItem("jwt") == null) {
-          next();
-        } else {
-          next({ name: "about" });
-        }
-      } else {
-        next();
-      }
-    });
-  }, //beforeMount
+				//check if matched route requires authentication
+				if (to.matched.some((record) => record.meta.requiresAuth)) {
+					//if matched route requires authentication then check for absence of token
+					if (localStorage.getItem("jwt") == null && !this.checkCookie("token")) {
+						//when no token is found redirect to login page and set redirec
+						next({
+							name: "login",
+							query: { nextUrl: to.name },
+						});
+					}
+					//if matched route requires authentication and has token
+					else {
+						let user = "{}";
+						if (localStorage.getItem("user")) {
+							user = localStorage.getItem("user");
+						} else if (this.checkCookie("user")) {
+							//console.log(this.getCookie('user'));
+							user = this.getCookie("user");
+						}
+						// const user = JSON.parse(localStorage.getItem('user') || JSON.parse(this.getCookie('user')) ||{});
+						//when token is present check if user is an Admin
+						if (to.matched.some((record) => record.meta.isAdmin)) {
+							//If user is an admin, proceed
+							if (user.isAdmin == 1) {
+								next();
+							}
+							//if user is not admin then redirect to  about page
+							else {
+								next({ path: "home" });
+							}
+						} else {
+							next();
+						}
+					}
+				}
+				//authentication was not required.
+				//check if guest access is required to matched route
+				else if (to.matched.some((record) => record.meta.guest)) {
+					if (localStorage.getItem("jwt") == null) {
+						next();
+					} else {
+						next({ name: "about" });
+					}
+				} else {
+					next();
+				}
+			});
+		}, //beforeMount
 
-  mounted() {
-    // document.addEventListener("scroll", this.scrollNav);
-  }, //mounted
+		mounted() {
+			// document.addEventListener("scroll", this.scrollNav);
+		}, //mounted
 
-  methods: {
-    // scrollNav: function(event) {
-    //   console.log("scrollNav", event);
-    // }, //scrollNav
-    beforeLeave(element) {
-      this.prevHeight = getComputedStyle(element).height;
-    }, //beforeLeave
-    enter(element) {
-      const { height } = getComputedStyle(element);
+		methods: {
+			// scrollNav: function(event) {
+			//   console.log("scrollNav", event);
+			// }, //scrollNav
+			beforeLeave(element) {
+				this.prevHeight = getComputedStyle(element).height;
+			}, //beforeLeave
+			enter(element) {
+				const { height } = getComputedStyle(element);
 
-      element.style.height = this.prevHeight;
+				element.style.height = this.prevHeight;
 
-      setTimeout(() => {
-        element.style.height = height;
-      });
-    }, //enter
-    afterEnter(element) {
-      element.style.height = "auto";
-    } //afterEnter
-  }
-};
+				setTimeout(() => {
+					element.style.height = height;
+				});
+			}, //enter
+			afterEnter(element) {
+				element.style.height = "auto";
+			}, //afterEnter
+		},
+	};
 </script>
 
 <style lang="less" scoped>
-@import (reference) "./../Less/customMixins.less";
-@import (reference) "./../Less/customVariables.less";
+	@import (reference) "./../Less/customMixins.less";
+	@import (reference) "./../Less/customVariables.less";
 
-.enterpriseAppLayout {
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  height: 100vh;
-  width: 100vw;
-  & > div {
-    display: flex;
-    flex-direction: row;
-    height: 100%;
-    &.body {
-      display: flex;
-      flex-direction: column;
-      max-width: 100vw;
-      .scroll(100vh);
+	.enterpriseAppLayout {
+		display: flex;
+		flex-direction: column;
+		position: relative;
+		height: 100vh;
+		width: 100vw;
+		& > div {
+			display: flex;
+			flex-direction: row;
+			height: 100%;
+			&.body {
+				display: flex;
+				flex-direction: column;
+				max-width: 100vw;
+				.scroll(100vh);
 
-      //scroll content
-      div {
-        &.content {
-          align-self: center;
-          color: @textColor;
-          max-width: 72vw;
-          min-width: 1024px;
-          padding: 0 @spaceLg;
-          // background-color: lightcyan;
-          //countdown timer
-          & > .alert {
-            flex-direction: row;
-            background-color: @dangerBorder;
-            padding: @spaceMd @spaceLg;
-            border-radius: @borderRadiusLg;
-            height: fit-content;
-            width: fit-content;
-          }
-        }
-      }
-    }
-  }
-  @media screen {
-    @media (max-width: @maxWidth) {
-      & > div {
-        &.body {
-          & > div.content {
-            max-width: 1024px;
-            min-width: 320px;
-          }
-        }
-      }
-    }
-  }
-}
+				//scroll content
+				div {
+					&.content {
+						align-self: center;
+						color: @textColor;
+						max-width: 72vw;
+						min-width: 1024px;
+						padding: 0 @spaceLg;
+						// background-color: lightcyan;
+						//countdown timer
+						& > .alert {
+							flex-direction: row;
+							background-color: @dangerBorder;
+							padding: @spaceMd @spaceLg;
+							border-radius: @borderRadiusLg;
+							height: fit-content;
+							width: fit-content;
+						}
+					}
+				}
+			}
+		}
+		@media screen {
+			@media (max-width: @maxWidth) {
+				& > div {
+					&.body {
+						& > div.content {
+							max-width: 1024px;
+							min-width: 320px;
+						}
+					}
+				}
+			}
+		}
+	}
 
-//zoom transition
-.zoom-enter-active,
-.zoom-leave-active {
-  animation-duration: @transitionDuration;
-  animation-fill-mode: both;
-  animation-name: zoom;
-}
+	//zoom transition
+	.zoom-enter-active,
+	.zoom-leave-active {
+		animation-duration: @transitionDuration;
+		animation-fill-mode: both;
+		animation-name: zoom;
+	}
 
-.zoom-leave-active {
-  animation-direction: reverse;
-}
+	.zoom-leave-active {
+		animation-direction: reverse;
+	}
 
-@keyframes zoom {
-  from {
-    opacity: 0;
-    transform: scale3d(0.5, 0.5, 0.5);
-  }
+	@keyframes zoom {
+		from {
+			opacity: 0;
+			transform: scale3d(0.5, 0.5, 0.5);
+		}
 
-  100% {
-    opacity: 1;
-  }
-}
+		100% {
+			opacity: 1;
+		}
+	}
 
-//fade transition
-.fade-enter-active,
-.fade-leave-active {
-  transition-duration: @transitionDuration;
-  transition-property: opacity;
-  transition-timing-function: @transitionTimingFunction;
-}
+	//fade transition
+	.fade-enter-active,
+	.fade-leave-active {
+		transition-duration: @transitionDuration;
+		transition-property: opacity;
+		transition-timing-function: @transitionTimingFunction;
+	}
 
-.fade-enter,
-.fade-leave-active {
-  opacity: 0;
-}
+	.fade-enter,
+	.fade-leave-active {
+		opacity: 0;
+	}
 
-//slide transition
-.slide-left-enter-active,
-.slide-left-leave-active,
-.slide-right-enter-active,
-.slide-right-leave-active {
-  transition-duration: @transitionDuration;
-  transition-property: height, opacity, transform;
-  transition-timing-function: @transitionTimingFunction;
-  overflow: hidden;
-}
+	//slide transition
+	.slide-left-enter-active,
+	.slide-left-leave-active,
+	.slide-right-enter-active,
+	.slide-right-leave-active {
+		transition-duration: @transitionDuration;
+		transition-property: height, opacity, transform;
+		transition-timing-function: @transitionTimingFunction;
+		overflow: hidden;
+	}
 
-.slide-left-enter,
-.slide-right-leave-active {
-  opacity: 0;
-  transform: translate(96px, 0);
-}
+	.slide-left-enter,
+	.slide-right-leave-active {
+		opacity: 0;
+		transform: translate(96px, 0);
+	}
 
-.slide-left-leave-active,
-.slide-right-enter {
-  opacity: 0;
-  transform: translate(-96px, 0);
-}
+	.slide-left-leave-active,
+	.slide-right-enter {
+		opacity: 0;
+		transform: translate(-96px, 0);
+	}
 </style>
--- src/views/project/js/scrollIndicator/scrollIndicator.vue
+++ src/views/project/js/scrollIndicator/scrollIndicator.vue
@@ -1,212 +1,233 @@
 <template>
-  <div class="scrollIndicator">
-    <div class="scrolContent">
-      <slot />
-    </div>
-    <div v-if="tagOffset.length > 1" class="scroll">
-      <a
-        v-for="indicator in tagOffset"
-        :key="indicator.id"
-        :href="'#' + indicator.id"
-        :class="{ active: indicator.selected }"
-      >
-        <span :class="[indicator.selected ? 'fas' : 'far', 'fa-circle']" />
-        <span>{{ indicator.id }}</span>
-      </a>
-    </div>
-  </div>
+	<div class="scrollIndicator">
+		<div class="scrolContent">
+			<slot />
+		</div>
+		<div v-if="tagOffset.length > 1" class="scroll">
+			<a
+				v-for="indicator in tagOffset"
+				:key="indicator.id"
+				:href="'#' + indicator.id"
+				:class="{ active: indicator.selected }"
+			>
+				<span :class="[indicator.selected ? 'fas' : 'far', 'fa-circle']" />
+				<span>{{ indicator.id }}</span>
+			</a>
+		</div>
+	</div>
 </template>
 <script>
-import { debounce } from "@/typeScript/debounce";
+	import { debounce } from "@/typeScript/debounce";
 
-export default {
-  name: "ScrollIndicator",
-  mixins: [debounce],
-  data() {
-    //used to calculate the height of header/navigation
-    const headerOffset = null;
-    //saves the sectrion tags used to generate Sub Content navigation links.
-    const tag = [];
-    //stores the tag (section) ad well as the top and bottom pixel location of the tag as well as if its selected or not.
-    const tagOffset = [];
-    //stores height opf the current browser window
-    const windowHeight = null;
-    //stores pixel value above which we select a tag.
-    const windowBuffer = null;
-    //ratio used to calculate window buffer
-    const bufferRatio = 0.24;
-    return {
-      headerOffset,
-      tag,
-      tagOffset,
-      windowHeight,
-      windowBuffer,
-      bufferRatio
-    };
-  },
-  mounted() {
-    this.initialize();
-  },
-  methods: {
-    checkScroll: function() {
-      const highlight = Math.round(this.headerOffset.scrollTop);
-      for (let i = 0; i < this.tag.length; i++) {
-        const tagOffset = this.tagOffset[i];
+	export default {
+		name: "ScrollIndicator",
+		mixins: [debounce],
+		data() {
+			//used to calculate the height of header/navigation
+			const headerOffset = null;
+			//saves the sectrion tags used to generate Sub Content navigation links.
+			const tag = [];
+			//stores the tag (section) ad well as the top and bottom pixel location of the tag as well as if its selected or not.
+			const tagOffset = [];
+			//stores height opf the current browser window
+			const windowHeight = null;
+			//stores pixel value above which we select a tag.
+			const windowBuffer = null;
+			//ratio used to calculate window buffer
+			const bufferRatio = 0.24;
+			const prevScrollValue = 0;
+			return {
+				headerOffset,
+				tag,
+				tagOffset,
+				windowHeight,
+				windowBuffer,
+				bufferRatio,
+				prevScrollValue,
+			};
+		},
+		mounted() {
+			this.initialize();
+		},
+		methods: {
+			checkScroll: function () {
+				const highlight = Math.round(this.headerOffset.scrollTop);
+				// if (highlight > 0) {
+				// 	console.log(
+				// 		"header",
+				// 		highlight,
+				// 		document.getElementsByTagName("header")[0].classList.add("mini")
+				// 	);
+				// document.getElementsByTagName('header')[0].innerHTML
+				if (this.prevScrollValue > highlight) {
+					document.getElementsByTagName("header")[0].classList.remove("mini");
+				} else {
+					if (
+						!document
+							.getElementsByTagName("header")[0]
+							.classList.contains("showNav")
+					) {
+						document.getElementsByTagName("header")[0].classList.add("mini");
+					}
+				}
+				// }
+				for (let i = 0; i < this.tag.length; i++) {
+					const tagOffset = this.tagOffset[i];
 
-        //activate tags depending on the window scroll location and tag top/bottom pixel location within the page.
-        if (
-          highlight + this.windowBuffer > tagOffset.top &&
-          highlight < tagOffset.bottom
-        ) {
-          tagOffset.selected = true;
-        } else {
-          //keep last tag selected when scroll exceeds bottom pixel location
-          if (
-            i == this.tag.length &&
-            this.windowHeight + highlight >=
-              this.tagOffset[this.tagOffset.length - 1].bottom + 32
-          ) {
-            tagOffset.selected = true;
-          } else {
-            tagOffset.selected = false;
-          }
-        }
-      }
-      this.prevScrollValue = highlight;
-    }, //checkScroll
-    initialize: function() {
-      this.headerOffset = document.getElementsByClassName("body")[0];
-      const offsetHeader = Array.from(
-        document.getElementsByClassName("vueHeader")
-      )[0];
-      this.tag = Array.from(document.getElementsByTagName("section"));
-      this.tag = this.tag.filter(tag => tag.id != "");
-      const headerOffset = offsetHeader ? offsetHeader.offsetHeight : 0;
-      this.tagOffset = this.tag.map(section => {
-        return {
-          top: section.offsetTop - headerOffset,
-          bottom: section.offsetTop + section.offsetHeight - headerOffset,
-          id: section.getAttribute("id"),
-          selected: false
-        };
-      });
-      if (this.tag.length > 1) {
-        this.headerOffset.addEventListener(
-          "scroll",
-          this.debounce(this.checkScroll),
-          {
-            capture: false, // top to bottom bubbling/propogation
-            once: false //should work only once
-          }
-        );
-        this.windowHeight = this.headerOffset.offsetHeight;
-        this.windowBuffer = this.windowHeight * this.bufferRatio;
-        this.checkScroll();
-      }
-    }
-  }
-};
+					//activate tags depending on the window scroll location and tag top/bottom pixel location within the page.
+					if (
+						highlight + this.windowBuffer > tagOffset.top &&
+						highlight < tagOffset.bottom
+					) {
+						tagOffset.selected = true;
+					} else {
+						//keep last tag selected when scroll exceeds bottom pixel location
+						if (
+							i == this.tag.length &&
+							this.windowHeight + highlight >=
+								this.tagOffset[this.tagOffset.length - 1].bottom + 32
+						) {
+							tagOffset.selected = true;
+						} else {
+							tagOffset.selected = false;
+						}
+					}
+				}
+				this.prevScrollValue = highlight;
+			}, //checkScroll
+			initialize: function () {
+				this.headerOffset = document.getElementsByClassName("body")[0];
+				const offsetHeader = Array.from(
+					document.getElementsByClassName("vueHeader")
+				)[0];
+				this.tag = Array.from(document.getElementsByTagName("section"));
+				this.tag = this.tag.filter((tag) => tag.id != "");
+				const headerOffset = offsetHeader ? offsetHeader.offsetHeight : 0;
+				this.tagOffset = this.tag.map((section) => {
+					return {
+						top: section.offsetTop - headerOffset,
+						bottom: section.offsetTop + section.offsetHeight - headerOffset,
+						id: section.getAttribute("id"),
+						selected: false,
+					};
+				});
+				if (this.tag.length > 1) {
+					this.headerOffset.addEventListener(
+						"scroll",
+						this.debounce(this.checkScroll),
+						{
+							capture: false, // top to bottom bubbling/propogation
+							once: false, //should work only once
+						}
+					);
+					this.windowHeight = this.headerOffset.offsetHeight;
+					this.windowBuffer = this.windowHeight * this.bufferRatio;
+					this.checkScroll();
+				}
+			},
+		},
+	};
 </script>
 <style lang="less" scoped>
-@import (reference) "../../../../Less/customMixins.less";
-@import (reference) "../../../../Less/customVariables.less";
+	@import (reference) "../../../../Less/customMixins.less";
+	@import (reference) "../../../../Less/customVariables.less";
 
-.scrollIndicator {
-  & > div {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    position: relative;
+	.scrollIndicator {
+		& > div {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			position: relative;
 
-    &.scroll {
-      position: absolute;
-      height: fit-content;
-      top: @header + @spaceLg;
-      right: @spaceLg*2;
-      &::before {
-        content: "Page Content";
-        text-align: right;
-        font-weight: bold;
-      }
-      & > a {
-        display: flex;
-        flex-direction: row-reverse;
-        align-items: center;
-        text-decoration: none;
-        padding: @spaceMd;
-        margin: @spaceSm 0;
-        height: @spaceXl;
-        background-color: transparent;
-        width: auto;
-        &:hover,
-        &.active {
-          border-radius: @borderRadius;
-          color: @white;
-          //active block text;
-          & > span:last-child {
-            display: block;
-            color: @textColor;
-            font-weight: bold;
-            margin-right: @spaceMd;
-          }
-        }
-        & > span {
-          .textShadow(@base);
-          &:last-child {
-            display: none;
-          }
-        }
-      }
-    }
+			&.scroll {
+				position: absolute;
+				height: fit-content;
+				top: @header + @spaceLg;
+				right: @spaceLg*2;
+				&::before {
+					content: "Page Content";
+					text-align: right;
+					font-weight: bold;
+				}
+				& > a {
+					display: flex;
+					flex-direction: row-reverse;
+					align-items: center;
+					text-decoration: none;
+					padding: @spaceMd;
+					margin: @spaceSm 0;
+					height: @spaceXl;
+					background-color: transparent;
+					width: auto;
+					&:hover,
+					&.active {
+						border-radius: @borderRadius;
+						color: @white;
+						//active block text;
+						& > span:last-child {
+							display: block;
+							color: @textColor;
+							font-weight: bold;
+							margin-right: @spaceMd;
+						}
+					}
+					& > span {
+						.textShadow(@base);
+						&:last-child {
+							display: none;
+						}
+					}
+				}
+			}
 
-    &.scrolContent {
-      display: flex;
-      align-self: center;
-      max-width: @maxWidth;
-    }
-  }
-  @media screen {
-    @media (max-width: @maxWidth) {
-      & > div {
-        &.scroll {
-          background-color: @accentColor;
-          border-radius: @spaceXl;
-          padding: @spaceXs;
-          top: 50%;
-          transform: translateY(-50%);
-          right: @spaceLg;
-          & > a {
-            width: fit-content;
-            &:hover,
-            &.active {
-              background-color: none;
-              .boxShadow(none);
-              & > span:last-child {
-                position: absolute;
-                width: max-content;
-                text-align: center;
-                color: @accentColor;
-                top: -40px;
-                right: -8px;
-                font-weight: bold;
-              }
-            }
-            & > span {
-              color: @backgroundColor;
-              .textShadow(@one);
-              &:last-child {
-                background-color: @backgroundColor;
-                width: 100%;
-                .textShadow(none);
-              }
-            }
-          }
-          &::before {
-            content: "";
-          }
-        }
-      }
-    }
-  }
-}
+			&.scrolContent {
+				display: flex;
+				align-self: center;
+				max-width: @maxWidth;
+			}
+		}
+		@media screen {
+			@media (max-width: @maxWidth) {
+				& > div {
+					&.scroll {
+						background-color: @accentColor;
+						border-radius: @spaceXl;
+						padding: @spaceXs;
+						top: 50%;
+						transform: translateY(-50%);
+						right: @spaceLg;
+						& > a {
+							width: fit-content;
+							&:hover,
+							&.active {
+								background-color: none;
+								.boxShadow(none);
+								& > span:last-child {
+									position: absolute;
+									width: max-content;
+									text-align: center;
+									color: @accentColor;
+									top: -40px;
+									right: -8px;
+									font-weight: bold;
+								}
+							}
+							& > span {
+								color: @backgroundColor;
+								.textShadow(@one);
+								&:last-child {
+									background-color: @backgroundColor;
+									width: 100%;
+									.textShadow(none);
+								}
+							}
+						}
+						&::before {
+							content: "";
+						}
+					}
+				}
+			}
+		}
+	}
 </style>
