4ecb87827d2e5de747daa5908db1e777851a621e
Hrishikesh karale <hrishikesh.karale@gmail.com>
2021-10-01 04:48:03 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,6 @@ const functions = require('firebase-functions');
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-//configure env
-require('dotenv').config();
-
-//twilio
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
-const accountSid = "ACc55f4375a5da3246b262df5a1875eb0c";
-const authToken = "32ea9117df10a25808383da8505a9885";
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 //api routes
 const apiRoutes = require("./api");
